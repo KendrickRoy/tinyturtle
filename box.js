9ae5c9a0eee0ff39908e9d5a344b895aabe93b1d
Kendrick Roy <roykendrick934@gmail.com>
2017-01-11 22:52:48 +0000

    notice megit status

---
+++ box.js
@@ -0,0 +1,31 @@
+TinyTurtle.apply(window); 
+function drawSquare(size){
+    forward(size);
+    right(90);
+    forward(size);
+    right(90);
+    forward(size);
+    right(90);
+    forward(size);
+}
+drawSquare(50)
+drawSquare(30)
+
+function drawTriangle(size){
+    left(120);
+    forward(size);
+    left(120);
+    forward(size);
+    left(120);
+    forward(size)
+}
+function drawRhombus(size){
+    forward(size)
+    right(30)
+    forward(size)
+    right(40)
+    forward(size)
+    Right(40)
+    forward(size)
+}
+drawRhombus(30)
